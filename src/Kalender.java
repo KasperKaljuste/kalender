fc1bbe0aa96a18b2e68480bc9105d31e82c5406a
KasperKaljuste <kasperk@tuta.io>
2021-03-25 20:04:44 +0200

    Lisasin kella ja kuupäeva classid, basic programm on olemas

--- src/Kalender.java
+++ src/Kalender.java
@@ -1,19 +1,30 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class Kalender {
     //Kuidas teha nii, et küsib lõpmatult kasutaja käest sisestust.
     public static void main(String[] args) {
+        Kell kell = new Kell();
+        Kuupäev kuupäev = new Kuupäev();
+        String state = new String();
+        state = ""; //Algne state on tühi ehk näitab kuupäeva
+        Scanner scanner = new Scanner(System.in);
+        List<Event> evendid = new ArrayList<>();
+
         //Väljastab evendid järjestatult compareTo
         //List<Event>
-        //for ea event kalendrist
+        //for each event kalendrist
         //Küsib sisestust, mida teha
         //Lisa event new Event
         //Täpsem info evendi kohta event.toString
-        // Kindla päeva evendid
+        //Kindla päeva evendid
         //Sobivad ajad?
         //Randomiga valib programm aja sobivate aegade seast
         //Otsi nime järgi
         //Kustuta event
         //getterid ja setterid iga asja kohta, lisa detaile jne
-
 
 
         //Meeldetuletusega evendi puhul: Event objekti asemel loo Meeldetuletus objekt
@@ -22,7 +33,48 @@ public class Kalender {
         //mis võrdleks Meeldetuletus objekti isendivälja LocalTime meeldetuletuseAeg väärtust
         //konstantselt java.time.LocalTime.now() väärtusega. Kui väärtused on võrdsed, siis väljastab
         //meeldetuletuse sõnumi.
+
+
+        while (true) {
+            if (state.equals("") || state.equals("kuupäev")) {
+                System.out.print("\r" + kuupäev.getTäna());
+                System.out.println();
+                state = scanner.nextLine(); //Mis state järgmises tsüklis läheb
+                System.out.println("Valisite: " + state);
+            }
+            else if (state.equals("kell")) {
+                System.out.print("\r" + kell.getKell());
+                System.out.println();
+                state = scanner.nextLine(); //Mis state järgmises tsüklis läheb
+                System.out.println("Valisite: " + state);
+            }
+            else if (state.equals("lisa")) { //Saab lisada evendi hetkel ainult ühe detailiga
+                System.out.println();
+                System.out.println("Sisesta ürituse nimi: ");
+                String nimi = scanner.nextLine();
+                System.out.println("Sisesta kuupäev: ");
+                String päev = scanner.nextLine();
+                System.out.println("Sisesta aeg: ");
+                String aeg = scanner.nextLine();
+                System.out.println("Sisesta detailid: ");
+                String detail = scanner.nextLine();
+                Event uus = new Event(nimi, päev, aeg, Arrays.asList(detail)) {
+                    @Override
+                    public void meeldetuletus(String päev, String aeg) {
+                    }
+                };
+                evendid.add(uus);
+                System.out.println(evendid);
+                state = ""; //Esialgsesse state tagasi
+            }
+            else{
+                state = ""; //Kui kirjutatakse mingi suvaline käsklus siis läheb ka esialgsesse state tagasi
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
-
-
 }
---
+++ src/Kuupäev.java
@@ -0,0 +1,14 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Kuupäev{
+    Date kuupäev = Calendar.getInstance().getTime();
+    DateFormat kuupäevaVorming = new SimpleDateFormat("EEEE, dd MMMM yyyy");
+    String täna = kuupäevaVorming.format(kuupäev);
+
+    public String getTäna() {
+        return täna;
+    }
+}
